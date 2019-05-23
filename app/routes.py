1ba68fc327f15c447685caced30e3cd15fb7ef2d
wsquinn <walkersquinn@comcast.net>
2019-05-22 23:14:10 -0700

    Added a home page

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,17 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
-@app.route('/index')
+@app.route('/homepage', methods=['GET', 'POST'])
+def homepage():
+	form = LoginForm()
+	if form.validate_on_submit():
+		return redirect(url_for('index'))
+	return render_template('homepage.html', title='Welcome', form=form)
+
+@app.route('/index', methods=['GET', 'POST'])
 def index():
 	
 	return render_template('index.html')
+
